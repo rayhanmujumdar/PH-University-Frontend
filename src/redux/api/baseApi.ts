65b6d9d71d924eb3f91904e96243cdf6325d572b
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2024-02-07 23:08:24 +0600

    Redux baseQuery implement to  refresh token expire validation

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,10 +1,13 @@
 import {
     BaseQueryApi,
+    BaseQueryFn,
+    DefinitionType,
     FetchArgs,
     createApi,
     fetchBaseQuery,
 } from "@reduxjs/toolkit/query/react";
-import { setUser } from "../features/auth/authSlice";
+import { toast } from "sonner";
+import { logout, setUser } from "../features/auth/authSlice";
 import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
@@ -12,7 +15,6 @@ const baseQuery = fetchBaseQuery({
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
         const token = (getState() as RootState).auth.token;
-        console.log({ token });
         if (token) {
             headers.set("authorization", token);
         }
@@ -20,12 +22,12 @@ const baseQuery = fetchBaseQuery({
     },
 });
 
-const baseQueryWithRefreshToken = async (
-    args: string | FetchArgs,
-    api: BaseQueryApi,
-    extraOptions: Record<string, unknown>
-) => {
-    const result = await baseQuery(args, api, extraOptions);
+const baseQueryWithRefreshToken: BaseQueryFn<
+    FetchArgs,
+    BaseQueryApi,
+    DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+    let result = await baseQuery(args, api, extraOptions);
     const errorStatus = result.error?.status;
     if (errorStatus === 401 || errorStatus === 403) {
         const res = await fetch(
@@ -36,10 +38,18 @@ const baseQueryWithRefreshToken = async (
             }
         );
         const getRefreshToken = await res.json();
-        const user = (api.getState() as RootState).auth.user;
-        api.dispatch(
-            setUser({ user, token: getRefreshToken.data.accessToken })
-        );
+        if (getRefreshToken.data?.accessToken) {
+            const user = (api.getState() as RootState).auth.user;
+            api.dispatch(
+                setUser({ user, token: getRefreshToken.data?.accessToken })
+            );
+            result = await baseQuery(args, api, extraOptions);
+        } else {
+            api.dispatch(logout());
+            toast.error("Access denied! Please login", {
+                className: "!text-red-500",
+            });
+        }
     }
     return result;
 };
